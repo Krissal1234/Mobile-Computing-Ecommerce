5b6262e4f95f6fcc47715c59ef1934a3460383ec
TimmyZammit <timmyzammit@icloud.com>
2023-12-07 18:36:23 +0100

    styling finished
    -fixed styling for:
       -home
       -Login
       -Register

--- store-frontend/src/Views/HomeScreen/styles.js
+++ store-frontend/src/Views/HomeScreen/styles.js
@@ -1,6 +1,16 @@
 import { StyleSheet } from 'react-native';
+import {colors} from '../colors'
 
 export default StyleSheet.create({
+        container:{
+            flex:1,
+            height:'100%',
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            backgroundColor: colors.darkBlue,
+        },    
         logo: {
         paddingVertical:'60%',
         width:'120%',
