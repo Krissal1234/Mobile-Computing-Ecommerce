212ed295d5eb1916669781b4f784e6136663e747
TimmyZammit <102827014+TimmyZammit@users.noreply.github.com>
2023-12-28 13:47:55 +0100

    Update firebase.js

--- store-frontend/config/firebase.js
+++ store-frontend/config/firebase.js
@@ -39,5 +39,3 @@ export const getAllListedSports = httpsCallable(functions, 'getAllListedSports')
 export const getPaymentSheet = httpsCallable(functions, 'getPaymentSheet');
 export const getCurrentOrders = httpsCallable(functions, 'getCurrentOrders');
 export const getPastOrders = httpsCallable(functions, 'getPastOrders');
-
-export const getPaymentSheet = httpsCallable(functions, 'getPaymentSheet');
