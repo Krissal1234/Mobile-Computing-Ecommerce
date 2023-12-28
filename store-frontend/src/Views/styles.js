f69182dc4126212b43117c207188a6ecfe09eb01
Zack-Mangani <zack.mangani.21@um.edu.mt>
2023-12-28 17:00:19 +0100

    styles arranged
    arranged styles

--- store-frontend/src/Views/styles.js
+++ store-frontend/src/Views/styles.js
@@ -28,8 +28,7 @@ export default StyleSheet.create({
         justifyContent:'center'
 
     },
-    title: {
-    },
+    
     logo: {
         height: height*0.3,
         width: width*0.4,
@@ -191,5 +190,85 @@ export default StyleSheet.create({
     testStyle:{
         marginHorizontal:width*0.1,
     },
+
+
+    Detailscontainer: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#132945',
+         // Set your desired background color
+      },
+      card: {
+        padding: 15,
+        marginBottom: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#A2383A',
+      },
+      detailsimage: {
+        width: '80%',
+        height: 200,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#A2383A',
+        marginBottom: 10,
+        alignSelf: 'center',
+      },
+      title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 5,
+        color: '#333',
+      },
+      description: {
+        color: '#555',
+        textAlign: 'center',
+      },
+      price: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 10,
+      },
+      subtitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+      },
+      switchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      switchText: {
+        marginRight: 10,
+      },
+      map: {
+        height: 200,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#A2383A',
+      },
+      detailsinput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        padding: 10,
+      },
+      dateTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        textAlign: 'center',
+      },
+      backButton: {
+       marginBottom: 10, 
+       paddingLeft:20,
+      },
+
+      
+    
 
 })
