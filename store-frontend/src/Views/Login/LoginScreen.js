c25cd6f1551687c808c4d36399766522e7546a3e
TimmyZammit <timmyzammit@icloud.com>
2024-01-07 12:59:53 +0100

    Revert "Merge branch 'main' of https://github.com/Krissal1234/Mobile-Computing-Ecommerce"
    This reverts commit 3c5a2e73d16d27885122da52b09e3a95f9c1bb5b, reversing
    changes made to 87d3f8eaef47b49f0e1223dc0b62ea2eb9ab40e0.

--- store-frontend/src/Views/Login/LoginScreen.js
+++ store-frontend/src/Views/Login/LoginScreen.js
@@ -1,17 +1,14 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Image, Text, TextInput, TouchableOpacity, SafeAreaView ,ActivityIndicator, View,Button} from 'react-native';
+import { Image, Text, TextInput, TouchableOpacity, SafeAreaView ,ActivityIndicator, View} from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from 'store-frontend/src/Views/styles';
-import {auth, googleSignIn, googleProvider, googleOAuthProvider} from '../../../config/firebase'
 import LoginController from '../../Controllers/LoginController';
 import { colors } from 'store-frontend/src/Views/colors.js';
 import { UserContext } from '../../Contexts/UserContext';
-import * as Google from "expo-auth-session/providers/google";
-import * as WebBrowser from "expo-web-browser";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { EquipmentController } from '../../Controllers/EquipmentController';
 import { ListingsController } from '../../Controllers/ListingsController';
-//import { ANDROID_CLIENT_ID, IOS_CLIENT_ID,  } from "../../../../.env";
+import { getEquipmentById } from '../../../config/firebase';
+import { getEquipmentListingsByUserUID } from '../../../config/firebase';
 
 
 export default function LoginScreen({ navigation }) {
@@ -21,87 +18,12 @@ export default function LoginScreen({ navigation }) {
   const {setSportCategories } = useContext(UserContext);
   const [loading, setLoading] = useState(false);
 
-     const config = {
-      androidClientId: "79967591982-96u6h31ffjjq8fmc3ortj7tqjflc1dhk.apps.googleusercontent.com",
-      iosClientId: "79967591982-kkuap9679v67qhrq4bl7h3mrjcsr6psd.apps.googleusercontent.com",
-      webClientId: "79967591982-24q93ovcvnd73t91k0tn1ph96ve9nqat.apps.googleusercontent.com",
-    };
-  const [request, response, promptAsync] = Google.useAuthRequest(config);
-
-  WebBrowser.maybeCompleteAuthSession();
   const onFooterLinkPress = () => {
     navigation.navigate('Registration');
   };
-  const getUserInfo = async (token) => {
-    //absent token
-    if (!token) return;
-    //present token
-    try {
-      const response = await fetch(
-        "https://www.googleapis.com/userinfo/v2/me",
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-      const user = await response.json();
-      //store user information  in Asyncstorage
-      await AsyncStorage.setItem("user", JSON.stringify(user));
-      setUserInfo(user);
-    } catch (error) {
-      console.error(
-        "Failed to fetch user data:",
-        response.status,
-        response.statusText
-      );
-    }
-  };
-  const signInWithGoogle = async () => {
-
-  googleSignIn(googleProvider)
-  .then((result) => {
-    /** @type {googleOAuthProvider} */
-    var credential = result.credential;
-
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // IdP data available in result.additionalUserInfo.profile.
-      // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-    // try {
-    //   // Attempt to retrieve user information from AsyncStorage
-    //   const userJSON = await AsyncStorage.getItem("user");
-  
-    //   if (userJSON) {
-    //     // If user information is found in AsyncStorage, parse it and set it in the state
-    //     setUserInfo(JSON.parse(userJSON));
-    //   } else if (response?.type === "success") {
-    //     // If no user information is found and the response type is "success" (assuming response is defined),
-    //     // call getUserInfo with the access token from the response
-    //     getUserInfo(response.authentication.accessToken);
-    //   }
-    // } catch (error) {
-    //   // Handle any errors that occur during AsyncStorage retrieval or other operations
-    //   console.error("Error retrieving user data from AsyncStorage:", error);
-    // }
-  };
-  
-  //add it to a useEffect with response as a dependency 
-  useEffect(() => {
-    signInWithGoogle();
-  }, [response]);
 
   const onLoginPress = async () => {
+
   setLoading(true);
     console.log('clicked login');
     const login = await LoginController.loginUser(email, password);
@@ -163,7 +85,7 @@ export default function LoginScreen({ navigation }) {
             />
             <TouchableOpacity
               style={styles.button}
-              onPress={() => signIn()}>
+              onPress={() => onLoginPress()}>
               <Text style={styles.buttonTitle}>Log in</Text>
             </TouchableOpacity>
             <SafeAreaView style={styles.footerView}>
@@ -173,7 +95,6 @@ export default function LoginScreen({ navigation }) {
                   Sign up
                 </Text>
               </Text>
-              <Button title= "sign in with google" onPress={()=>{signInWithGoogle()}}/>
             </SafeAreaView>
           </>
         )}
